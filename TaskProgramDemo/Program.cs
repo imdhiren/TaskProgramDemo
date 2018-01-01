244ae739f961edac9c0344523127a8b07175da0e
Dhiren Patel <Dhiren Patel>
2018-01-01 21:23:12 +0530

    Add project files.

---
+++ TaskProgramDemo/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TaskProgramDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IntroducingTasks.CreateAndStartSimpleTasks();
+            IntroducingTasks.TasksWithState();
+            IntroducingTasks.TasksWithReturnValues();
+
+            Console.WriteLine("Main program done, press any key.");
+            Console.ReadKey();
+        }
+    }
+}
